26659731a273bb6023f2b0f311da3777cc1ab4ff
Juraj Carnogursky <durisvk2@gmail.com>
2022-04-24 21:51:20 +0200

    v0.0.1-alpha.8

--- packages/core/src/fetcher.ts
+++ packages/core/src/fetcher.ts
@@ -1,24 +1,28 @@
-import fetch from 'cross-fetch';
-import type { GraphQLResult, Fetcher, FetcherInput, VariablesType } from './types';
+import type {
+  GraphQLResult,
+  Fetcher,
+  FetcherInput,
+  VariablesType,
+} from "./types";
 
-export type DefaultFetcherOptions = Omit<RequestInit, 'body'> & { url: string };
+export type DefaultFetcherOptions = Omit<RequestInit, "body"> & { url: string };
 export type FetcherOptions<
   TResult extends GraphQLResult<object> = GraphQLResult<object>,
-  TVariables extends VariablesType = VariablesType,
+  TVariables extends VariablesType = VariablesType
 > = Fetcher<TResult, TVariables> | DefaultFetcherOptions;
 
 export const defaultFetcher = <
   TResult extends GraphQLResult = GraphQLResult,
-  TVariables extends VariablesType = VariablesType,
+  TVariables extends VariablesType = VariablesType
 >({
   url,
   ...options
 }: DefaultFetcherOptions): Fetcher<TResult, TVariables> => {
   return async (input: FetcherInput<TVariables>) => {
     const res = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       ...options,
       body: JSON.stringify({
